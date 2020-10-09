ebc5ece15be940e19fa0eab9f615dc5ac34dad2e
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-10-10 00:45:45 +0800

    Set MVC with packages for basic signUp/In

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const createError = require('http-errors')
 const express = require('express')
 // 判別開發環境，這行放最前面才能運作
 if (process.env.NODE_ENV !== 'production') {
@@ -8,10 +7,22 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
-const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
+const db = require('./models') // 引入資料庫
+
+const exphbs = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
+const passport = require('./config/passport')
 
 const app = express()
+
+// 設定 view engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: 'main'
+}))
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -19,13 +30,25 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+// 設定 session
+app.use(session({
+  secret: 'secret',
+  cookie: { maxAge: 86400000 }, // 延長到一天， 以免用到一半被登出
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
 })
+
+// 設定 passport
+app.use(passport.initialize())
+app.use(passport.session())
 
 // error handler
 app.use(function (err, req, res, next) {
@@ -37,5 +60,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500)
   res.render('error')
 })
+
+require('./routes')(app, passport) // 把 passport 傳入 routes
 
 module.exports = app
